b2e94ee5e2c456d9f122146903596cbfc43559f8
AppiahAberde <jonakhme@gmail.com>
2021-01-28 16:22:36 +0000

    Adding Screen

--- App.js
+++ App.js
@@ -1,18 +1,26 @@
 import 'react-native-gesture-handler';
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { NavigationContainer } from '@react-navigation/native'
+import { NavigationContainer, StackActions } from '@react-navigation/native'
+import { createStackNavigator } from '@react-navigation/stack'
 import { StyleSheet, Text, View } from 'react-native';
+import Homescreen from './screens/Homescreen';
+import Detailscreen from './screens/Detailscreen'
+
+const Stack = createStackNavigator()
 
 export default function App() {
   return (
     <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name='Home' component={Homescreen} />
+        <Stack.Screen name = 'Details'  component={Detailscreen}/>
+      </Stack.Navigator>
       <View style={styles.container}>
         <Text>Open up App.js to start working on your app!</Text>
         <StatusBar style="auto" />
       </View>
     </NavigationContainer>
-
   );
 }
 
